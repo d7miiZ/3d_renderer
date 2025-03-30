7235457dfd8a2f0fb684024eb4f70a47c173930a
Abdulrahman Alshawi <ashawi@wirefilter.com>
2025-03-30 04:18:37 +0300

    chore: add cube and project it using simple projection & render it

--- src/main.c
+++ src/main.c
@@ -3,6 +3,12 @@
 #include <stdlib.h>
 
 #include <display.h>
+#include <vector.h>
+
+#define CUBE_DIMENSION 9 * 9 * 9
+
+vector3_t cube[CUBE_DIMENSION];
+vector2_t projected_cube[CUBE_DIMENSION];
 
 bool is_running = false;
 
@@ -17,6 +23,16 @@ bool setup(void) {
     if (!texture) {
         fprintf(stderr, "Error in creating texture %s\n", SDL_GetError());
         return false;
+    }
+    
+    uint16_t count = 0;
+    for (float x = -1; x <= 1; x += 0.25) {
+        for (float y = -1; y <= 1; y += 0.25) {
+            for (float z = -1; z <= 1; z += 0.25) {
+                vector3_t vec = { .x = x, .y = y, .z = z};
+                cube[count++] = vec;
+            }
+        }
     }
     
     return true;
@@ -40,15 +56,21 @@ void process_input(void) {
     }
 }
 
-void update(void) { }
+void update(void) {
+    for (size_t i = 0; i < CUBE_DIMENSION; i++) {
+        projected_cube[i] = orthographic_projection(cube[i]);
+    }
+}
 
 void render(void) {
-    SDL_SetRenderDrawColor(renderer, 255, 255, 0, 255);
-    SDL_RenderClear(renderer);
+    draw_grid(20);
 
+    for (size_t i = 0; i < CUBE_DIMENSION; i++) {
+        vector2_t projection = projected_cube[i];
+        draw_rect(projection.x, projection.y, 4, 4, 0xFFFFFF00);
+    }
+    
     render_color_buffer();
-    draw_grid(30);
-    draw_rect(300, 200, 0xFF0000FF);
 
     SDL_RenderPresent(renderer);
 }
