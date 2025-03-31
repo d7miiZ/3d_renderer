fea5999555cdebc42099e59b4df50c16e70e66c7
Abdulrahman Alshawi <ashawi@wirefilter.com>
2025-03-31 16:19:54 +0300

    chore: add basic perspective_projection & fix divide by zero issue & add camera

--- src/vector.c
+++ src/vector.c
@@ -1,9 +1,26 @@
 #include <vector.h>
 #include <display.h>
 
-#define FOV_FACTOR 128
+vector2_t orthographic_projection(vector3_t vec3, uint32_t fov_factor) {
+    vector2_t vec2;
+    if (vec3.z == 0) {
+        vec2.x = window_height / 2;
+        vec2.y = window_width / 2;
+    } else {
+        vec2.x = (fov_factor * vec3.x) + (window_height / 2);
+        vec2.y = (fov_factor * vec3.y) + (window_width / 2);
+    }
+    return vec2;
+}
 
-vector2_t orthographic_projection(vector3_t vec3) {
-    vector2_t vec2 = { .x = (FOV_FACTOR * vec3.x) + (window_width / 2), .y = (FOV_FACTOR * vec3.y) + (window_height / 2)};
+vector2_t perspective_projection(vector3_t vec3, uint32_t fov_factor) {
+    vector2_t vec2;
+    if (vec3.z == 0) {
+        vec2.x = window_height / 2;
+        vec2.y = window_width / 2;
+    } else {
+        vec2.x = ((fov_factor * vec3.x) / vec3.z) + (window_height / 2);
+        vec2.y = ((fov_factor * vec3.y) / vec3.z) + (window_width / 2);
+    }
     return vec2;
 }
