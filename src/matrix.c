754aee51588313f9e7ea0aa814399221ddc0e2e7
d7miiZ <noonecares@care.com>
2025-09-01 18:00:22 +0300

    chore: define matrix

---
+++ src/matrix.c
@@ -0,0 +1,20 @@
+#include <matrix.h>
+
+mat4_t mat4_identity() {
+    mat4_t m = {{
+        {1, 0, 0, 0},
+        {0, 1, 0, 0},
+        {0, 0, 1, 0},
+        {0, 0, 0, 1},
+    }};
+
+    return m;
+}
+
+mat4_t mat4_scale(float sx, float sy, float sz) {
+    mat4_t m = mat4_identity();
+    m.m[0][0] = sx;
+    m.m[1][1] = sy;
+    m.m[2][2] = sz;
+    return m;
+}
